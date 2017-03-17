5fd20dc17063f6e8154adcf483dbaed09b602d97
LiamXero <liamtownsley.coder@gmail.com>
2017-03-17 21:32:39 +0000

    Update 2 (#2)
    * Delete avatar.png
    
    * Delete index.html
    
    * Delete login.html
    
    * Delete main.css
    
    * Add files via upload
    
    * Create main.css
    
    * Create main.js
    
    * Create temp
    
    * Add files via upload
    
    * Delete temp

---
+++ js/main.js
@@ -0,0 +1,9 @@
+		function gotoindex(time){
+			window.setTimeout(function() {
+       			window.location.href='index.html';
+    		}, time * 1000);
+		}
+
+		var timer = setTimeout(function() {
+        window.location='index.html'
+        }, 10000);
